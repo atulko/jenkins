c47e11abac23fae3b0502896b772a192dfbc8abb
ATUL <atul.koypare@gmail.com>
2021-10-15 17:07:12 +0530

    controller added

---
+++ src/main/java/com/example/jenkins/controller/ApplicationController.java
@@ -0,0 +1,19 @@
+package com.example.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest")
+public class ApplicationController {
+	
+	
+	
+	@GetMapping("/hello")
+	public String getMessege()
+	{
+		return "Hello World";
+	}
+
+}
